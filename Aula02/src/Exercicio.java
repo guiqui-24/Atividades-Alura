e273ef8ecf72c356117fa9667628c66d05e04599
guiqui-24 <g.garciaa2406@gmail.com>
2025-11-27 22:58:28 -0300

    Aula 02 e exercicio

---
+++ Aula02/src/Exercicio.java
@@ -0,0 +1,8 @@
+public class Exercicio {
+    public static void main(String[] args) {
+        // Conversão de celsius para farenheit
+        int celsius = 24;
+        double fahrenheit = (celsius * 1.8) + 32;
+        System.out.println("Fahrenheit: " + fahrenheit);
+    }
+}
